d812515fe46cbd2156e467ca0d1c2dacc6416242
bgn9000 <bgn9000@ubuntu.ubuntu-domain>
2012-04-18 22:19:56 +0200

    some more optimizations in scripts (gokhanmoral)

--- initramfs/ICS/sam-initramfs/sbin/ext/post-init.sh
+++ initramfs/ICS/sam-initramfs/sbin/ext/post-init.sh
@@ -5,7 +5,38 @@
 #exec >>/data/user.log
 #exec 2>&1
 
-/sbin/busybox sh /sbin/ext/modules.sh
+. /res/customconfig/customconfig-helper
+read_defaults
+read_config
+
+mkdir /data/.shun
+chmod 777 /data/.shun
+[ ! -f /data/.shun/default.profile ] && cp /res/customconfig/default.profile /data/.shun
+[ ! -f /data/.shun/battery.profile ] && cp /res/customconfig/battery.profile /data/.shun
+[ ! -f /data/.shun/performance.profile ] && cp /res/customconfig/performance.profile /data/.shun
+
+if [ "$logger" == "on" ];then
+insmod /lib/modules/logger.ko
+fi
+
+# disable debugging on some modules
+if [ "$logger" == "off" ];then
+  rm -rf /dev/log
+  echo 0 > /sys/module/ump/parameters/ump_debug_level
+  echo 0 > /sys/module/mali/parameters/mali_debug_level
+  echo 0 > /sys/module/kernel/parameters/initcall_debug
+  echo 0 > /sys//module/lowmemorykiller/parameters/debug_level
+  echo 0 > /sys/module/wakelock/parameters/debug_mask
+  echo 0 > /sys/module/userwakelock/parameters/debug_mask
+  echo 0 > /sys/module/earlysuspend/parameters/debug_mask
+  echo 0 > /sys/module/alarm/parameters/debug_mask
+  echo 0 > /sys/module/alarm_dev/parameters/debug_mask
+  echo 0 > /sys/module/binder/parameters/debug_mask
+  echo 0 > /sys/module/xt_qtaguid/parameters/debug_mask
+fi
+
+# for ntfs automounting
+insmod /lib/modules/fuse.ko
 
 #/sbin/busybox sh /sbin/ext/busybox.sh
 
@@ -28,9 +59,12 @@ sleep 30
 /sbin/busybox sh /sbin/ext/efs-backup.sh
 ) &
 
+# apply ExTweaks defaults
+/res/uci.sh apply &
+
 ##### init scripts #####
 (
-sleep 12
+sleep 10
 /sbin/busybox sh /sbin/ext/run-init-scripts.sh
 )&
 
