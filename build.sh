0f0365cf7fc5f0f6c655bc0737cad19a938cd85d
bgn9000 <bgn9000@ubuntu.ubuntu-domain>
2012-07-01 21:34:36 +0200

    Go back to GCC 4.6.3 until 4.7.1 will be a real release

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ export ARCH=arm
 #export CONFIG_CC_OPTIMIZE_FOR_SPEED=1
 export CONFIG_CC_OPTIMIZE_FOR_FAST=1
 # GCC 4.6.3
-# export CROSS_COMPILE=$PARENT_DIR/arm-2012/bin_463/arm-linux-gnueabi-
+export CROSS_COMPILE=$PARENT_DIR/arm-2012/bin_463/arm-linux-gnueabi-
 # GCC 4.7.1
-export CROSS_COMPILE=$PARENT_DIR/arm-2012/bin_471/arm-linux-gnueabihf-
+#export CROSS_COMPILE=$PARENT_DIR/arm-2012/bin_471/arm-linux-gnueabihf-
 
 echo config...
 if [ ! -f $KERNELDIR/.config ];
