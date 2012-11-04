cfcbc563e9efc8719b493ebd8bdeb2b8772f2dfa
bgn9000 <bgn9000@bgn9000-UX31E.(none)>
2012-11-05 00:30:53 +0100

    Revert "genalloc: stop crashing the system when destroying a pool"
    This reverts commit 3052916546a28ca67a3edb2844c8579d857447f3.

--- lib/genalloc.c
+++ lib/genalloc.c
@@ -79,9 +79,7 @@ gen_pool_add_virt(struct gen_pool *pool, unsigned long virt, phys_addr_t phys,
 		  size_t size, int nid)
 {
 	struct gen_pool_chunk *chunk;
-	int nbits = size >> pool->min_alloc_order;
-	int nbytes = sizeof(struct gen_pool_chunk) +
-				BITS_TO_LONGS(nbits) * sizeof(long);
+	size_t nbytes;
 
 	if (WARN_ON(!virt || virt + size < virt ||
 		    (virt & ((1 << pool->order) - 1))))
